3fdf1c313a22fd94995e20cb0faa91d87ea22ebb
jeroentiggelman <infos@photographicdesign.be>
2020-07-27 16:34:32 +0200

    added plugin react helmet and correction site img

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
